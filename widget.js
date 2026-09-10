529e725a9b6c3bef40f55febca25777136138ef0
JFP44 <33450591+JFP44@users.noreply.github.com>
2026-09-10 07:43:20 +0000

    Corriger colonne SERVICE DEMANDEUR

--- widget.js
+++ widget.js
@@ -912,7 +912,7 @@ var columnMapping = {
     group: 'Group_Name',
     startDate: 'Start_Date',
     dueDate: 'Due_Date',
-    serviceDemandeur: 'SERVICE_DEMANDEUR',
+    serviceDemandeur: 'SERVICE DEMANDEUR',
     tag: 'Tag',
     recurrence: 'Recurrence',
     estimatedHours: 'Estimated_Hours',
